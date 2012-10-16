d548bc840c6d951749901b9cbb7a394cbc9ae911
Damian Suarez <rdsuarez@gmail.com>
2012-10-16 10:38:04 -0300

    test: add tests

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var slugize = require('../');
+
+describe('slugize(string, 20)', function(){
+  it('should return a string equal to \'josip-radosevic\'', function(){
+    slugize('Josip Radošević').should.be.equal('josip-radosevic');
+  });
+
+  it('should return a string equal to \'le-lys-sucree\'', function(){
+    slugize('lë lys sucrée').should.be.equal('le-lys-sucree');
+  });
+});
